2d4143042afe59e4d87f559a341d24cfcd060460
bitterteddy <87904121+bitterteddy@users.noreply.github.com>
2024-05-12 15:43:37 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,10 @@
+# Use a non-existing directory for ease of testing
+import appdirs as ad
+
+ad.user_cache_dir = lambda *args: ".cache"
+
+# If the ad.user_cache_dir directory doesn't exist, this fails
+import yfinance as yf
 import streamlit as st
 import pandas as pd
 import numpy as np
