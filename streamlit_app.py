3c70db8d19f4b553fbbe65893d6b034b37b1aa8a
bitterteddy <87904121+bitterteddy@users.noreply.github.com>
2024-05-12 14:51:50 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,14 @@
+import numpy as np
+import pandas as pd
+import yfinance as yf
+from keras.models import load_model
+import streamlit as st
+import matplotlib.pyplot as plt
 
+st.header('Stock Market Predictor')
+
+stock =st.text_input('Enter Stock Symnbol', 'GOOG')
+start = '2012-01-01'
+end = '2022-12-31'
+
+st.subheader('Stock Data')
